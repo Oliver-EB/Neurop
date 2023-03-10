6d9c82947eb05e88e990c37d29ae57c3b2c1afee
Oliver-EB <85533586+Oliver-EB@users.noreply.github.com>
2023-03-09 22:11:43 -0600

    Modulo ConfPerfil

---
+++ src/app/ConfPerfil/confperfil.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+selector : 'confperfil',
+templateUrl : './confperfil.component.html',
+styleUrls : ['./confperfil.component.css']
+})
+
+export class confperfil{
+    
+}
